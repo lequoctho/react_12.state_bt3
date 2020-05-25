2a840fb88481aecdd42ae1acd76d5bb6db250f66
Glitch (starter-react-template) <none>
2020-05-25 15:45:45 +0000

    💍💒 Checkpoint ./src/components/SearchBox.js:15532837/1383 ./src/App.js:15532837/70 ./package.json:15532837/982 ./src/components/SearchBox.css:15532837/38

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBox from './components/SearchBox';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          
+        <SearchBox/>  
       </div>
     );
   }
---
+++ src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import classNames from 'classnames';
+import './SearchBox.css'
+
+class SearchBox extends Component {
+    
+    render() {
+        //const { currentColor } = this.props;
+        return <div className='SearchBox'>
+                <input type="text" />
+            </div>;
+    }
+}
+
+export default SearchBox;
