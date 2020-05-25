b2faa925a10bc6071391e2d74ac2eb462b622a85
Glitch (starter-react-template) <none>
2020-05-25 16:09:40 +0000

    👗🌆 Checkpoint ./src/components/SearchBox.css:15532837/255 ./src/components/SearchBox.js:15532837/34

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -8,8 +8,8 @@ class SearchBox extends Component {
         //const { currentColor } = this.props;
         return <div className='SearchBox'>
                 <input type="text" />
-                <img src="https://cdn.glitch.com/3916b47e-07d9-47aa-b61c-db84c74ee7bb%2FEllipse%203.png?v=1590421667794"/>
-                <img src="https://cdn.glitch.com/3916b47e-07d9-47aa-b61c-db84c74ee7bb%2Fsearch%201.png?v=1590422058964"/>    
+                <img className="hide" src="https://cdn.glitch.com/3916b47e-07d9-47aa-b61c-db84c74ee7bb%2FEllipse%203.png?v=1590421667794"/>
+                <img className="hide" src="https://cdn.glitch.com/3916b47e-07d9-47aa-b61c-db84c74ee7bb%2Fsearch%201.png?v=1590422058964"/>    
             </div>;
     }
 }
